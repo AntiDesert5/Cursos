fcd90d02a861b804d78ee46f7b64fb29b55b8bf1
AntiDesert5 <axelvalenzuela7@gmail.com>
2019-05-24 07:21:54 -0500

    conectando las vistas y poniendo imagen de muestra

--- app/src/main/java/com/example/cursos/Datos.java
+++ app/src/main/java/com/example/cursos/Datos.java
@@ -1,36 +1,102 @@
 package com.example.cursos;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Datos extends AppCompatActivity implements View.OnClickListener {
-    private Button aceptar;
-    private AutoCompleteTextView nombre,appat,apmat,email,contra,centrouni;
+    private static final String PATH_MENSAJES = "Mensajes";
+
+    @BindView(R.id.Nombre)
+    AutoCompleteTextView Nombre;
+    @BindView(R.id.ApPat)
+    AutoCompleteTextView ApPat;
+    @BindView(R.id.ApMat)
+    AutoCompleteTextView ApMat;
+    @BindView(R.id.centrouni)
+    AutoCompleteTextView centrouni;
+    @BindView(R.id.Emaildat)
+    AutoCompleteTextView Emaildat;
+    @BindView(R.id.Password)
+    AutoCompleteTextView Password;
+    @BindView(R.id.aceptar)
+    Button aceptar;
+    private static String PATH_USER ="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
-        aceptar = (Button) findViewById(R.id.aceptar);
-        nombre =(AutoCompleteTextView)findViewById(R.id.Nombre);
-        appat =(AutoCompleteTextView)findViewById(R.id.ApPat);
-        apmat =(AutoCompleteTextView)findViewById(R.id.ApMat);
-        email =(AutoCompleteTextView)findViewById(R.id.Emaildat);
-        contra =(AutoCompleteTextView)findViewById(R.id.Password);
-        centrouni =(AutoCompleteTextView)findViewById(R.id.centrouni);
+        ButterKnife.bind(this);
         aceptar.setOnClickListener(this);
     }
 
+    public List values(){
+        List<String> Datos = new ArrayList<String>();
+        Datos.add(Nombre.getText().toString());
+        Datos.add(ApPat.getText().toString());
+        return Datos;
+    }
+
+    public void conexion(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference= database.getReference(PATH_MENSAJES);
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        PATH_USER=Emaildat.getText().toString();
+        switch (v.getId()) {
             case (R.id.aceptar):
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference reference= database.getReference(PATH_MENSAJES).child(PATH_USER);
+
                 Toast.makeText(this, "Registro Creado", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(Datos.this,MainActivity.class);
+                Intent intent = new Intent(Datos.this, MainActivity.class);
                 startActivity(intent);
                 finish();
                 break;
